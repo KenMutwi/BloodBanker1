a086d18d45b30499a07de124193f5af95befe38f
KenMutwi <kennethy.mutwiri@gmail.com>
2020-03-11 09:43:51 +0300

    third commit

--- app/src/main/java/com/example/bloodbanker/Activities/MainActivity.java
+++ app/src/main/java/com/example/bloodbanker/Activities/MainActivity.java
@@ -2,20 +2,31 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.bloodbanker.Adapters.RequestAdapter;
+import com.example.bloodbanker.DataModels.RequestDataModel;
 import com.example.bloodbanker.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
             public class MainActivity extends AppCompatActivity {
+                private RecyclerView recyclerView;
+                private List<RequestDataModel> requestDataModelList;
+                private RequestAdapter requestAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestDataModelList = new ArrayList<>();
         Toolbar toolbar= findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
@@ -31,6 +42,22 @@ import com.example.bloodbanker.R;
                 return false;
             }
         });
+        recyclerView = findViewById(R.id.recyclerView);
+        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(this,RecyclerView.VERTICAL,false);
+        recyclerView.setLayoutManager(layoutManager);
+        requestAdapter = new RequestAdapter(requestDataModelList,this);
+        recyclerView.setAdapter(requestAdapter);
+        populateHomePage();
+    }
+    private void populateHomePage(){
+        RequestDataModel requestDataModel= new RequestDataModel("I love you sweet Purity Gakii Gikunda","https://www.pexels.com/photo/affection-afterglow-backlit-blur-556667/");
+        requestDataModelList.add(requestDataModel);
+        requestDataModelList.add(requestDataModel);
+        requestDataModelList.add(requestDataModel);
+        requestDataModelList.add(requestDataModel);
+        requestDataModelList.add(requestDataModel);
+        requestAdapter.notifyDataSetChanged();
+
     }
 
                 @Override
